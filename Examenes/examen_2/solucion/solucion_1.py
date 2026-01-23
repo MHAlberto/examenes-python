987b08130d71042264412d15aacd67c440f2e547
Merio07 <mayiitoo_alberto@hotmail.com>
2026-01-23 05:55:30 -0600

    Sesión 2 fue un exito en mejorar el segundo examén, pero no tuve la oprtunidad de mejorar en el tema de su solución, me enfoque tanto en mejorarlo que si lo hice dificil a tal punto que me cuesta demasiado resolverlo

--- Examenes/examen_2/solucion/solucion_1.py
+++ Examenes/examen_2/solucion/solucion_1.py
@@ -10,3 +10,9 @@ def normalizacion_texto(texto:str)->str:
     
 texto = "Python \n  es\t\tgenial\n2026"
 print(normalizacion_texto(texto))
+
+
+"Sesión 2 fue un exito en mejorar el segundo examén,
+pero no tuve la oprtunidad de mejorar en el tema de 
+su solución, me enfoque tanto en mejorarlo que si
+lo hice dificil a tal punto que me cuesta demasiado resolverlo"
